ca64bd09f18fa5ab2aece72168305ecd51021a69
Blessed <blessed15896@gmail.com>
2024-06-24 10:05:21 +0200

    Canonical URL for Post model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -40,3 +41,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
+
