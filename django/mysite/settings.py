623a339c224273e19fa78437580232a950dd4d85
Blessed <blessed15896@gmail.com>
2024-06-25 11:48:50 +0200

    Remove sqlite database entry

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -80,10 +80,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
